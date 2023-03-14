001f516cb5cfec4ca3abdaa45a193c66da99be4b
David <davojh27@gmail.com>
2023-03-13 19:02:11 -0500

    clase 162

--- src/modules/daybook/routes/index.js
+++ src/modules/daybook/routes/index.js
@@ -5,7 +5,17 @@ export default {
     name:'daybook',
     component: () => import(/* webpackChunkName: "DayBook" */ '@/modules/daybook/layouts/DayBookLayout.vue'),
     children: [
-        
+        {
+            path: '',
+            name: 'no-entry',
+            component: () => import(/* webpackChunkName: "DayBook" */ '@/modules/daybook/views/NoEntrySelected.vue'),
+        },
+        {
+            path: ':id',
+            name: 'entry',
+            component: () => import(/* webpackChunkName: "DAybook-no-entry" */ '@/modules/daybook/views/EntryView.vue'),
+
+        }
     ]
 
 }
